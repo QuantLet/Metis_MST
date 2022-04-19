3e0bce0a429542ebed37523f873781f034aa20f3
wzj5163 <81182810+wzj5163@users.noreply.github.com>
2022-04-19 03:28:10 +0200

    Add files via upload

---
+++ MST_sp500/MST_sp500.m
@@ -0,0 +1,100 @@
+%% TiME SERIES by Kruskal
+clc,clear
+load("sp500.mat");
+logret=sample(:,4);
+price=sample(:,5);
+timeline=datenum(sample(:,1:3));
+% %% plot data set
+% figure;
+% subplot(2,1,1);
+% plot(timeline,price);
+% dateaxis('x',12);
+% xlim([timeline(1),timeline(end)]);
+% set(gca,'color','none','ytick',[],'yticklabel',[]);
+% subplot(2,1,2);
+% plot(timeline,logret);
+% dateaxis('x',12);
+% xlim([timeline(1),timeline(end)]);
+% set(gca,'color','none','ytick',[],'yticklabel',[]);
+% saveas(gcf,'PriRet_SP','png');
+%% plot scatter price and return
+figure;
+subplot(2,1,1);
+scatter(timeline,price,0.3,MarkerEdgeColor=[0,0.5,0.5])
+dateaxis('x',12);
+xlim([timeline(1),timeline(end)]);
+set(gca,'color','none','ytick',[],'yticklabel',[]);
+subplot(2,1,2);
+scatter(timeline,logret,0.3)
+dateaxis('x',12);
+xlim([timeline(1),timeline(end)]);
+set(gca,'color','none','ytick',0);
+% set(gca,'color','none','ytick',[0],'yticklabel',[]);
+%% save scatter price and return
+saveas(gcf,'PriceReturn_SP_scatter','png');
+%% plot scatter return
+figure;
+scatter(timeline,logret,0.3)
+dateaxis('x',12);
+xlim([timeline(1),timeline(end)]);
+set(gca,'color','none','ytick',[],'xticklabel',[],'XColor','none','YColor','none');
+%% save scatter return
+saveas(gcf,'Return_SP_scatter','png');
+%% generate minimal spanning tree
+DM=pdist2(logret,logret);
+for i=1:length(DM)-2
+    for j=i+2:length(DM)
+        DM(i,j)=inf;
+    end
+end
+for j=1:length(DM)-2
+    for i=j+2:length(DM)
+        DM(i,j)=inf;
+    end
+end
+G=graph(DM);
+T=minspantree(G,'Method','sparse');
+P=[timeline,logret];
+T_Edges=sortrows(T.Edges,"Weight");
+%% plot png of SP500 MST
+figure;
+for i = 1:length(T_Edges.Weight)
+    plot([P(T_Edges.EndNodes(i,1),1),P(T_Edges.EndNodes(i,2),1)]...
+        ,[P(T_Edges.EndNodes(i,1),2),P(T_Edges.EndNodes(i,2),2)],'r',"LineWidth",0.5);hold on
+end
+dateaxis('x',12);
+xlim([timeline(1),timeline(end)]);
+set(gca,'color','none','xticklabel',[],'XColor','none','YColor','none')
+%% save png of SP500 MST
+saveas(gcf,'MST_sp500','png');
+%% figure gif of SP500 return by Kruskal
+figure;
+ax=gca;
+ax.XColor='none';
+ax.YColor='none';
+hold(ax,'on')
+DelayTime=.001;
+scatter(timeline,logret,0.3)
+dateaxis('x',12);
+xlim([timeline(1),timeline(end)]);
+pause(DelayTime)
+F=getframe(ax);
+[imind,cm]=rgb2ind(F.cdata,256);
+imwrite(imind,cm,'SP500.gif','gif','Loopcount',inf,'DelayTime',DelayTime);
+for i = 1:length(T_Edges.Weight)
+    plot([P(T_Edges.EndNodes(i,1),1),P(T_Edges.EndNodes(i,2),1)]...
+        ,[P(T_Edges.EndNodes(i,1),2),P(T_Edges.EndNodes(i,2),2)],'r',"LineWidth",0.5);
+    if mod(i,100)==0
+        pause(DelayTime);saveFrame(ax,DelayTime);
+    end
+end
+pause(DelayTime);saveFrame(ax,DelayTime);
+
+function saveFrame(ax,DelayTime)
+F=getframe(ax);
+[imind,cm]=rgb2ind(F.cdata,256);
+imwrite(imind,cm,'SP500.gif','gif','WriteMode','append','DelayTime',DelayTime,"TransparentColor",0);
+end
+
+
+
